143627a7f9f9fd9c67bb6c93008c24814784d38e
Willian <Willian-0liveira>
2026-08-11 10:45:00 -0300

    Primeiros comandos em Dart

---
+++ Aula02-IntroducaoDart/meu-app/bin/meu_app.dart
@@ -0,0 +1,26 @@
+void main() {
+  print("Hello World!!");
+  int idade = 19;
+  double altura = 1.75;
+  String nome = "Willian";
+  bool ativo = true;
+
+print("Nome: $nome");
+print("Idade: $idade");
+print("Altura: $altura");
+print("Ativo: $ativo");
+
+//tipagem dinamica
+
+//var x dynamic
+//o var nao aceita mudanca
+
+var sobrenome = "Oliveira";
+var peso = 53;
+
+//o dynamic aceita mudanca no tipo
+
+dynamic terceironome = "Santos";
+terceironome = 100;
+terceironome= 10.00;
+}
